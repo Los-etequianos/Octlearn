e70140687d5cb6a62968cd3275e04014963da3f7
Grupo Los Etequianos <125988093+Los-etequianos@users.noreply.github.com>
2023-02-26 22:54:44 +0000

    Arrumando a navbar

--- js/menu-index.js
+++ js/menu-index.js
@@ -2,7 +2,7 @@ const menu = document.querySelector('nav');
 
 function activeScrool(){
     menu.classList.toggle('ativo', scrollY > 550);
-    menu.classList.toggle('roxo', scrollY > 2630);
+    menu.classList.toggle('roxo', scrollY > 2640);
 }
 
 window.addEventListener('scroll', activeScrool);
