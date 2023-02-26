96aa30df1caad3013d41eb9074b3c68361c51756
Grupo Los Etequianos <125988093+Los-etequianos@users.noreply.github.com>
2023-02-26 23:33:25 +0000

    finalizando index e sobre o projeto

--- js/menu-index.js
+++ js/menu-index.js
@@ -3,7 +3,7 @@ const menu = document.querySelector('nav');
 function activeScrool(){
     menu.classList.toggle('ativo', scrollY > 550);
     menu.classList.toggle('roxo', scrollY > 2610);
-    menu.classList.toggle('mudar_roxo', scrollY > 2670);
+    menu.classList.toggle('mudar_roxo', scrollY > 2590);
 }
 
 window.addEventListener('scroll', activeScrool);
