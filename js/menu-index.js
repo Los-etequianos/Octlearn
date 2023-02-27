f1de6c762c113911a8276432e0c57cf988305b41
Grupo Los Etequianos <125988093+Los-etequianos@users.noreply.github.com>
2023-02-27 02:10:02 +0000

    Modificando sobre o grupo

--- js/menu-index.js
+++ js/menu-index.js
@@ -4,6 +4,6 @@ function activeScrool(){
     menu.classList.toggle('ativo', scrollY > 550);
     menu.classList.toggle('roxo', scrollY > 2610);
     menu.classList.toggle('mudar_roxo', scrollY > 2600);
+    menu.classList.toggle('roxo_grupo', scrollY > 6160);
 }
-
 window.addEventListener('scroll', activeScrool);
